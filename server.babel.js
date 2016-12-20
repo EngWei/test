244467a56ac9126c5740a70c406b4214ee33188b
Eng Wei <ewleow@hotmail.com>
2016-12-20 14:46:02 +0800

    Sample of Web server using Express

---
+++ server.babel.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+app.use('/', express.static('public'));
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
